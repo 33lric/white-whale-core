a1189aa486fc240485897b1a693838c1f57d5e8c
Edward E <e3lric@outlook.com>
2023-03-29 11:43:46 +0000

    feat: add cargo features to lib.rs

--- packages/white-whale/src/lib.rs
+++ packages/white-whale/src/lib.rs
@@ -1,6 +1,12 @@
 pub mod fee;
+#[cfg(feature = "rewards")]
 pub mod fee_collector;
+#[cfg(feature = "rewards")]
 pub mod fee_distributor;
+#[cfg(feature = "dex")]
 pub mod pool_network;
+#[cfg(feature = "flashloans")]
 pub mod vault_network;
+#[cfg(feature = "bonding")]
 pub mod whale_lair;
+
